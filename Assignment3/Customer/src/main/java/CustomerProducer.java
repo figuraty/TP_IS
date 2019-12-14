759d81d7c04233e263ad4b89ef137259f62836a3
franciscoazedo97@gmail.com <franciscoazedo97@gmail.com>
2019-12-14 21:23:12 +0000

    Kafka streams untill point 12 working and tested

--- Assignment3/Customer/src/main/java/CustomerProducer.java
+++ Assignment3/Customer/src/main/java/CustomerProducer.java
@@ -24,7 +24,7 @@ public class CustomerProducer extends Thread {
     public void run(){
 
 //        String topicName = "salesTopic";
-        String topicName = "test42";
+        String topicName = "testTopic22";
         Properties props = new Properties();
         props.put("bootstrap.servers", "localhost:9092");
         props.put("acks", "all");
--- Assignment3/KafkaStreamsProcessor/src/main/java/KafkaStreamsProcessor.java
+++ Assignment3/KafkaStreamsProcessor/src/main/java/KafkaStreamsProcessor.java
@@ -7,7 +7,6 @@ import org.apache.kafka.streams.kstream.*;
 
 import java.io.IOException;
 import java.time.Duration;
-import java.util.ArrayList;
 import java.util.Properties;
 
 public class KafkaStreamsProcessor {
@@ -16,11 +15,11 @@ public class KafkaStreamsProcessor {
         String purchasesTopic = "purchasesTopic";
         String salesTopic = "salesTopic";
         String testTopic = "testTopic20";
-        String test2Topic = "testTopic21";
-        String test3Topic = "test42";
+        String test2Topic = "testTopic22";
+        String test3Topic = "testTopic30";
 
         Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "exercises-application-15");
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "exercises-application-35");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
@@ -31,7 +30,7 @@ public class KafkaStreamsProcessor {
         KStream<String, String> purchasesData = builder.stream(testTopic);
 
         KStream<String, String> profitData = salesData.outerJoin(purchasesData,
-                KafkaStreamsProcessor::getProfit,
+                KafkaStreamsProcessor::getProfitPerItem,
                 JoinWindows.of(Duration.ofNanos(1)),
                 Joined.with(
                         Serdes.String(), /* key */
@@ -48,37 +47,46 @@ public class KafkaStreamsProcessor {
                         Serdes.String())  /* right value */
         );
 
-        KTable<String, Integer> revenuesPerItem = salesData.mapValues(KafkaStreamsProcessor::getRevenuesPerItem)
-                .groupByKey(Grouped.with(Serdes.String(), Serdes.Integer()))
-                .reduce(Integer::sum);
 
-        KTable<String, Integer> totalRevenues = salesData.mapValues(KafkaStreamsProcessor::getRevenuesPerItem)
-                .groupBy((k, v) -> "totalRevenues", Grouped.with(Serdes.String(), Serdes.Integer()))
-                .reduce(Integer::sum);
+        KTable<String, String> revenuesPerItem = salesData.mapValues(KafkaStreamsProcessor::getRevenuesPerItem)
+                .groupByKey(Grouped.with(Serdes.String(), Serdes.String()))
+                .reduce(KafkaStreamsProcessor::addRevenuesPerItem);
 
-        KTable<String, Integer> expensesPerItem = purchasesData.mapValues(KafkaStreamsProcessor::getExpensesPerItem)
-                .groupByKey(Grouped.with(Serdes.String(), Serdes.Integer()))
-                .reduce(Integer::sum);
+        
+        KTable<String, String> expensesPerItem = purchasesData.mapValues(KafkaStreamsProcessor::getExpensesPerItem)
+                .groupByKey(Grouped.with(Serdes.String(), Serdes.String()))
+                .reduce(KafkaStreamsProcessor::addExpensesPerItem);
 
-        KTable<String, Integer> totalExpenses = purchasesData.mapValues(KafkaStreamsProcessor::getExpensesPerItem)
-                .groupBy((k, v) -> "totalExpenses", Grouped.with(Serdes.String(), Serdes.Integer()))
-                .reduce(Integer::sum);
 
-        KTable<String, Integer> averageExpensesPerItem = purchasesData.mapValues(KafkaStreamsProcessor::getAverageExpensesPerItem)
-                .groupByKey(Grouped.with(Serdes.String(), Serdes.Integer()))
-                .reduce((v1, v2) -> (v1 + v2)/2 );
+        KTable<String, String> profitPerItem = profitData.groupByKey(Grouped.with(Serdes.String(), Serdes.String()))
+                .reduce(KafkaStreamsProcessor::addProfitPerItem);
 
-        KTable<String, Integer> totalAverageExpenses = purchasesData.mapValues(KafkaStreamsProcessor::getAverageExpensesPerItem)
-                .groupBy((k, v) -> "totalAverageExpenses", Grouped.with(Serdes.String(), Serdes.Integer()))
-                .reduce((v1, v2) -> (v1 + v2)/2 );
 
-        KTable<String, Integer> profitPerItem = profitData.mapValues(KafkaStreamsProcessor::transformStringToInt)
-                .groupByKey(Grouped.with(Serdes.String(), Serdes.Integer()))
-                .reduce(Integer::sum);
+        KTable<String, String> totalRevenues = salesData.mapValues(KafkaStreamsProcessor::getRevenuesPerItem)
+                .groupBy((k, v) -> "totalRevenues", Grouped.with(Serdes.String(), Serdes.String()))
+                .reduce(KafkaStreamsProcessor::addTotalRevenues);
 
-        KTable<String, Integer> totalProfit = profitData.mapValues(KafkaStreamsProcessor::transformStringToInt)
-                .groupBy((k, v) -> "totalProfit", Grouped.with(Serdes.String(), Serdes.Integer()))
-                .reduce(Integer::sum);
+
+        KTable<String, String> totalExpenses = purchasesData.mapValues(KafkaStreamsProcessor::getExpensesPerItem)
+                .groupBy((k, v) -> "totalExpenses", Grouped.with(Serdes.String(), Serdes.String()))
+                .reduce(KafkaStreamsProcessor::addTotalExpenses);
+        
+
+        KTable<String, String> totalProfit = profitData
+                .groupBy((k, v) -> "totalProfit", Grouped.with(Serdes.String(), Serdes.String()))
+                .reduce(KafkaStreamsProcessor::addTotalProfit);
+        
+        
+        KTable<String, String> averageExpensesPerItem = purchasesData.mapValues(KafkaStreamsProcessor::getAverageExpensesPerItem)
+                .groupByKey(Grouped.with(Serdes.String(), Serdes.String()))
+                .reduce(KafkaStreamsProcessor::calculateAverageExpensesPerItem);
+        
+
+        KTable<String, String> totalAverageExpenses = purchasesData.mapValues(KafkaStreamsProcessor::getAverageExpensesPerItem)
+                .groupBy((k, v) -> "totalAverageExpenses", Grouped.with(Serdes.String(), Serdes.String()))
+                .reduce(KafkaStreamsProcessor::calculateTotalAverageExpenses);
+
+
 
 //        KTable<String, String> testProfitPerItem = itemProfitData
 //                .mapValues(KafkaStreamsProcessor::transformData)
@@ -89,21 +97,26 @@ public class KafkaStreamsProcessor {
 
 
 
-        revenuesPerItem.mapValues(v -> transformData(v, "")).toStream().to(purchasesTopic, Produced.with(Serdes.String(), Serdes.String()));
+//        revenuesPerItem.toStream().to(test3Topic, Produced.with(Serdes.String(), Serdes.String()));
 
-//        totalRevenues.mapValues(KafkaStreamsProcessor::transformIntToString).toStream().to(purchasesTopic, Produced.with(Serdes.String(), Serdes.String()));
+//        expensesPerItem.toStream().to(test3Topic, Produced.with(Serdes.String(), Serdes.String()));
 
-//        expensesPerItem.mapValues(KafkaStreamsProcessor::transformIntToString).toStream().to(purchasesTopic, Produced.with(Serdes.String(), Serdes.String()));
+//        profitPerItem.toStream().to(test3Topic, Produced.with(Serdes.String(), Serdes.String()));
 
-//        totalExpenses.mapValues(KafkaStreamsProcessor::transformIntToString).toStream().to(purchasesTopic, Produced.with(Serdes.String(), Serdes.String()));
+//        totalRevenues.toStream().to(test3Topic, Produced.with(Serdes.String(), Serdes.String()));
 
-//        averageExpensesPerItem.mapValues(KafkaStreamsProcessor::transformIntToString).toStream().to(purchasesTopic, Produced.with(Serdes.String(), Serdes.String()));
+//        totalExpenses.toStream().to(test3Topic, Produced.with(Serdes.String(), Serdes.String()));
 
-//        totalAverageExpenses.mapValues(KafkaStreamsProcessor::transformIntToString).toStream().to(purchasesTopic, Produced.with(Serdes.String(), Serdes.String()));
+//        totalProfit.toStream().to(test3Topic, Produced.with(Serdes.String(), Serdes.String()));
 
-//        profitPerItem.mapValues(KafkaStreamsProcessor::transformIntToString).toStream().to(purchasesTopic, Produced.with(Serdes.String(), Serdes.String()));
+//        averageExpensesPerItem.toStream().to(test3Topic, Produced.with(Serdes.String(), Serdes.String()));
 
-//        totalProfit.mapValues(KafkaStreamsProcessor::transformIntToString).toStream().to(purchasesTopic, Produced.with(Serdes.String(), Serdes.String()));
+//        totalAverageExpenses.toStream().to(test3Topic, Produced.with(Serdes.String(), Serdes.String()));
+
+
+
+
+
 
 
         KafkaStreams streams = new KafkaStreams(builder.build(), props);
@@ -112,33 +125,37 @@ public class KafkaStreamsProcessor {
 
 
 
-    private static int getRevenuesPerItem(String message){
+    private static String getRevenuesPerItem(String message){
         Sale saleObject = new Gson().fromJson(message, Sale.class);
-
-        ReadTopicObject readTopicObject = new ReadTopicObject();
-        Schema schema = new Schema();
-        schema.setType("struct");
-        schema.setOptional(false);
-        ArrayList<Field> fields = new ArrayList<>();
-        fields.add(new Field("string", false, "name"));
-        fields.add(new Field("int", false, "revenues"));
-        Payload payload = new Payload();
-
-
-        return saleObject.getNumberOfUnits() * saleObject.getUnitPrice();
+        return "name:" + saleObject.getItem() + ", revenues:" + (saleObject.getNumberOfUnits() * saleObject.getUnitPrice());
     }
 
-    private static int getExpensesPerItem(String message){
+    private static String addRevenuesPerItem(String item1, String item2){
+
+        String name = item1.split(":")[1].split(",")[0];
+        int revenuesItem1 = Integer.parseInt(item1.split(":")[2]);
+        int revenuesItem2 = Integer.parseInt(item2.split(":")[2]);
+
+        //ALTERAR O RETORNO PARA UM OBJETO JSON
+        return "name:" + name + ", revenues:" + String.valueOf(revenuesItem1 + revenuesItem2);
+    }
+
+    private static String getExpensesPerItem(String message){
         Purchase purchaseObject = new Gson().fromJson(message, Purchase.class);
-        return purchaseObject.getNumberOfUnits() * purchaseObject.getUnitPrice();
+        return "name:" + purchaseObject.getItem() + ", expenses:" + (purchaseObject.getNumberOfUnits() * purchaseObject.getUnitPrice());
     }
 
-    private static int getAverageExpensesPerItem(String message){
-        Purchase purchaseObject = new Gson().fromJson(message, Purchase.class);
-        return purchaseObject.getUnitPrice();
+    private static String addExpensesPerItem(String item1, String item2){
+
+        String name = item1.split(":")[1].split(",")[0];
+        int expensesItem1 = Integer.parseInt(item1.split(":")[2]);
+        int expensesItem2 = Integer.parseInt(item2.split(":")[2]);
+
+        //ALTERAR O RETORNO PARA UM OBJETO JSON
+        return "name:" + name + ", expenses:" + String.valueOf(expensesItem1 + expensesItem2);
     }
 
-    private static String getProfit(String sale, String purchase){
+    private static String getProfitPerItem(String sale, String purchase){
         Sale saleObject = null;
         Purchase purchaseObject = null;
 
@@ -149,12 +166,84 @@ public class KafkaStreamsProcessor {
             purchaseObject = new Gson().fromJson(purchase, Purchase.class);
 
         if (saleObject == null)
-            return String.valueOf(purchaseObject.getNumberOfUnits() * purchaseObject.getUnitPrice() * (-1));
+            return "name:" + purchaseObject.getItem() + ", profit:" + purchaseObject.getNumberOfUnits() * purchaseObject.getUnitPrice() * (-1);
         else if (purchaseObject == null)
-            return String.valueOf(saleObject.getNumberOfUnits() * saleObject.getUnitPrice());
+            return "name:" + saleObject.getItem() + ", profit:" + saleObject.getNumberOfUnits() * saleObject.getUnitPrice();
 
-        return String.valueOf((saleObject.getNumberOfUnits() * saleObject.getUnitPrice()) - (purchaseObject.getNumberOfUnits() * purchaseObject.getUnitPrice()));
+        return "name:" + saleObject.getItem() + ", profit:" + ((saleObject.getNumberOfUnits() * saleObject.getUnitPrice()) - (purchaseObject.getNumberOfUnits() * purchaseObject.getUnitPrice()));
     }
+
+    private static String addProfitPerItem(String item1, String item2){
+
+        String name = item1.split(":")[1].split(",")[0];
+        int profitItem1 = Integer.parseInt(item1.split(":")[2]);
+        int profitItem2 = Integer.parseInt(item2.split(":")[2]);
+
+        //ALTERAR O RETORNO PARA UM OBJETO JSON
+        return "name:" + name + ", profit:" + (profitItem1 + profitItem2);
+    }
+
+    private static String addTotalRevenues(String item1, String item2){
+
+        int revenuesItem1 = Integer.parseInt(item1.split(":")[2]);
+        int revenuesItem2 = Integer.parseInt(item2.split(":")[2]);
+
+        //ALTERAR O RETORNO PARA UM OBJETO JSON
+        return "name:TotalRevenues, revenues:" + (revenuesItem1 + revenuesItem2);
+    }
+
+    private static String addTotalExpenses(String item1, String item2){
+
+        int expensesItem1 = Integer.parseInt(item1.split(":")[2]);
+        int expensesItem2 = Integer.parseInt(item2.split(":")[2]);
+
+        //ALTERAR O RETORNO PARA UM OBJETO JSON
+        return "name:TotalExpenses, expenses:" + (expensesItem1 + expensesItem2);
+    }
+
+    private static String addTotalProfit(String item1, String item2){
+
+        int profitItem1 = Integer.parseInt(item1.split(":")[2]);
+        int profitItem2 = Integer.parseInt(item2.split(":")[2]);
+
+        //ALTERAR O RETORNO PARA UM OBJETO JSON
+        return "name:TotalProfit, profit:" + (profitItem1 + profitItem2);
+    }
+
+    private static String getAverageExpensesPerItem(String message){
+        Purchase purchaseObject = new Gson().fromJson(message, Purchase.class);
+        return "name:" + purchaseObject.getItem() + ", averageExpenses:" + (purchaseObject.getNumberOfUnits() * purchaseObject.getUnitPrice() + ".0");
+    }
+
+    private static String calculateAverageExpensesPerItem(String item1, String item2){
+
+        String name = item1.split(":")[1].split(",")[0];
+        double expensesItem1 = Double.parseDouble(item1.split(":")[2]);
+        double expensesItem2 = Double.parseDouble(item2.split(":")[2]);
+        double avgExpenses = (expensesItem1 + expensesItem2) / 2;
+
+        //ALTERAR O RETORNO PARA UM OBJETO JSON
+        return "name:" + name + ", averageExpenses:" + avgExpenses;
+    }
+
+    private static String calculateTotalAverageExpenses(String item1, String item2){
+        
+        double expensesItem1 = Double.parseDouble(item1.split(":")[2]);
+        double expensesItem2 = Double.parseDouble(item2.split(":")[2]);
+        double avgExpenses = (expensesItem1 + expensesItem2) / 2;
+
+        //ALTERAR O RETORNO PARA UM OBJETO JSON
+        return "name:TotalAverageExpenses, averageExpenses:" + avgExpenses;
+    }
+
+
+
+
+
+//    private static int getAverageExpensesPerItem(String message){
+//        Purchase purchaseObject = new Gson().fromJson(message, Purchase.class);
+//        return purchaseObject.getUnitPrice();
+//    }
 
     private static String getItemsProfit(String sale, String purchase){
         Sale saleObject = null;
@@ -167,27 +256,10 @@ public class KafkaStreamsProcessor {
             purchaseObject = new Gson().fromJson(purchase, Purchase.class);
 
         if (saleObject == null)
-            return String.valueOf(purchaseObject.getItem() + ": " + purchaseObject.getNumberOfUnits() * purchaseObject.getUnitPrice() * (-1));
+            return purchaseObject.getItem() + ": " + purchaseObject.getNumberOfUnits() * purchaseObject.getUnitPrice() * (-1);
         else if (purchaseObject == null)
-            return String.valueOf(saleObject.getItem() + ": " + saleObject.getNumberOfUnits() * saleObject.getUnitPrice());
+            return saleObject.getItem() + ": " + saleObject.getNumberOfUnits() * saleObject.getUnitPrice();
 
-        return String.valueOf(saleObject.getItem() + ": " + ((saleObject.getNumberOfUnits() * saleObject.getUnitPrice()) - (purchaseObject.getNumberOfUnits() * purchaseObject.getUnitPrice())));
+        return saleObject.getItem() + ": " + ((saleObject.getNumberOfUnits() * saleObject.getUnitPrice()) - (purchaseObject.getNumberOfUnits() * purchaseObject.getUnitPrice()));
     }
-
-
-
-    private static int transformStringToInt(String number){
-        return Integer.parseInt(number);
-    }
-
-    private static String transformData(int number, String name){
-        System.out.println(number);
-        return "1";
-    }
-
-    private static String transformIntToString(int number){
-        return String.valueOf(number);
-    }
-
-
 }
